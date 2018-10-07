bddf5e35e31293c06fa5536dc0d070729c6c4a0a
Gareth Ellis <gareth.ellis0@gmail.com>
2018-10-07 10:23:55 -0700

    Made a StatementList Class

--- src/main/ForStatement.kt
+++ src/main/ForStatement.kt
@@ -2,7 +2,7 @@ class ForStatement(forString: String) : Statement(forString) {
 
     lateinit var id: IdNode
     lateinit var nodeSet: NodeSet
-    lateinit var statements: Array<Statement>
+    lateinit var statements: StatementList
 
     init {
         //TODO: parse forString and initialize properties

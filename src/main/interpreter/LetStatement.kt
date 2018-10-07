81c455263752549ec9abe5fea8d9422af3cbdabb
lorenzodb1 <lorenzodb1@hotmail.it>
2018-10-07 15:43:54 -0700

    Made LetStatement runnable, added comments with issues to be solved

--- src/main/interpreter/LetStatement.kt
+++ src/main/interpreter/LetStatement.kt
@@ -1,33 +1,29 @@
 package interpreter
 
+import Utils.STATEMENT_DIVIDER_REGEX
+import tokenizer.Grammar.isLetKey
+
 class LetStatement(letString: String): Statement(letString) {
 
-    lateinit var id: String
+    lateinit var id: String //TODO - lorenzodb1: Why is this a String and not a IdNode?
     lateinit var value: Node
 
-    private val statementDividerRegex: Regex = Regex("""\s""")
-
     init {
-        val letIterator = forString.split(statementDividerRegex).iterator()
-        val word = letIterator.next()
-        while (letIterator.hasnext()) {
-            if (isLetKey(word)) {
-                id = IdNode(word.next())
-                val equals = letIterator.next()
-                if (isEquals(equals)) {
-                    value = letIterator.next()
-                }//TODO: else, there is no equals sign here, throw an error
-                //TODO: else, there is no "Let", throw an error
-
+        val letStatementIterator = letString.split(STATEMENT_DIVIDER_REGEX).iterator()
+        val key = letStatementIterator.next()
+        while (letStatementIterator.hasNext()) {
+            if (isLetKey(key)) {
+                id = letStatementIterator.next()
+                val equals = letStatementIterator.next() //TODO - lorenzodb1: Assumption that the format is $X = 5
+                if (true) {
+                    //value = Node(letStatementIterator.next()) TODO - lorenzodb1 - Node is abstract!
+                }//TODO - leticianakajima: else, there is no equals sign here, throw an error
+                //TODO - leticianakajima: else, there is no "Let", throw an error
             }
-
-
         }
     }
 
-    public override fun interp() {
+    override fun interp() {
         //TODO: implement
     }
 }
-
-

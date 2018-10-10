7f054437f5a048bded4cc9bb33d193358ad04b6b
le-nakajima <lecantal@hotmail.com>
2018-10-10 13:39:02 -0700

    email_?

--- src/main/interpreter/GuestNode.kt
+++ src/main/interpreter/GuestNode.kt
@@ -1,10 +1,17 @@
 package interpreter
+import utils.Constant.STATEMENT_DIVIDER_REGEX
 
 class GuestNode(emailString: String) : Node(emailString) {
 
     lateinit var email: String
 
     init {
+        val emailIterator = letString.split(STATEMENT_DIVIDER_REGEX).iterator()
+        val key = emailIterator.next()
+        while (email.hasNext()) {
+            
+
+        }
         //TODO: validate emailString and initialize email
     }
 

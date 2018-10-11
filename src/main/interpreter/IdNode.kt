75b5c4db824da35c84a677aaa9d9a0af8c29294e
Gareth Ellis <gareth.ellis0@gmail.com>
2018-10-10 18:33:41 -0700

    Implemented and Tested IdNode

--- src/main/interpreter/IdNode.kt
+++ src/main/interpreter/IdNode.kt
@@ -2,7 +2,26 @@ package interpreter
 
 class IdNode(val idString: String) : Node(idString) {
 
+    lateinit var id: String;
+
+    init {
+        // TODO: check if reserved word?
+        if (idString.length < 2 || idString[0] != '$'){
+            throw ParseException("Invalid string given to IdNode: \"" + idString + "\"")
+        }
+
+        id = idString.substring(1)
+    }
+
     public override fun interp() {
         //TODO: implement
+    }
+
+    // TODO: Lots of copy-pasta between `equals` methods, can we use DRY?
+    override fun equals(other: Any?): Boolean {
+        return when (other) {
+            is IdNode -> other.id.equals(this.id)
+            else -> false
+        }
     }
 }
---
+++ src/test/IdNodeTest.kt
@@ -0,0 +1,66 @@
+import interpreter.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.TestInstance
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS) // This annotation is required for @BeforeAll to work
+class IdNodeTest {
+
+    // Test constructing a simple FOR loop with an empty Nodeset and no statements
+//    @Test
+//    fun constructForStatement_empty_nodeset_and_empty_statements() {
+//        var forString = "FOR \$X IN {} ENDFOR\n"
+//
+//        var forStatement = ForStatement(forString);
+//
+//        assertEquals(IdNode("\$X"), forStatement.id)
+//        assertEquals(NodeSet("{}"), forStatement.nodeSet)
+//        assertEquals(StatementList(""), forStatement.statements);
+//    }
+
+    @Test
+    fun constructIdNode_single_character_uppercase() {
+        var idString = "\$X"
+        var idNode = IdNode(idString)
+        assertEquals("X", idNode.id)
+    }
+
+    @Test
+    fun constructIdNode_single_character_lowercase() {
+        var idString = "\$x"
+        var idNode = IdNode(idString)
+        assertEquals("x", idNode.id)
+    }
+
+    @Test
+    fun constructIdNode_multiple_character_uppercase() {
+        var idString = "\$XYZ"
+        var idNode = IdNode(idString)
+        assertEquals("XYZ", idNode.id)
+    }
+
+    @Test
+    fun constructIdNode_multiple_character_lowercase() {
+        var idString = "\$xyz"
+        var idNode = IdNode(idString)
+        assertEquals("xyz", idNode.id)
+    }
+
+    @Test
+    fun constructIdNode_missing_dollar_sign() {
+        var idString = "x"
+        val exception = assertThrows(ParseException::class.java) {
+            IdNode(idString)
+        }
+    }
+
+    @Test
+    fun constructIdNode_missing_id() {
+        var idString = "$"
+        val exception = assertThrows(ParseException::class.java) {
+            IdNode(idString)
+        }
+    }
+}

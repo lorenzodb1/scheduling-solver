92704b78d9ad62ed34ff80bc8447027ab322a9b0
lorenzodb1 <lorenzodb1@hotmail.it>
2018-10-10 23:51:31 -0700

    Added EMAIL_REGEX

--- src/main/utils/Constant.kt
+++ src/main/utils/Constant.kt
@@ -3,5 +3,8 @@ package utils
 object Constant {
 
     internal val STATEMENT_DIVIDER_REGEX: Regex = Regex("""/\{([^}]+)\}|([^\s]+)/g""")
-    internal val COMMA_REGEX: Regex = Regex("/([^,]+)/g")
+
+    internal val COMMA_REGEX: Regex = Regex("""/([^,]+)/g""")
+
+    internal val EMAIL_REGEX: Regex = Regex("""^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))${'$'}""")
 }

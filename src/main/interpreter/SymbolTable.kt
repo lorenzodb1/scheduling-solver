621de3b97dbbbf23da15e621fd4acb6e498c8e02
Gareth Ellis <gareth.ellis0@gmail.com>
2018-10-13 17:52:23 -0700

    Added iterator to SymbolTable

--- src/main/interpreter/SymbolTable.kt
+++ src/main/interpreter/SymbolTable.kt
@@ -1,19 +1,21 @@
 package interpreter
 
-import interpreter.Node
-
 class SymbolTable {
-    private val table = HashMap<String, Node>()
+    private val table = HashMap<String, String>()
 
-    fun get(key: String) : Node {
+    fun get(key: String) : String {
         return table[key]!!
     }
 
-    fun set(key: String, value: Node) {
+    fun set(key: String, value: String) {
         table[key] = value
     }
 
     fun clear() {
         table.clear()
+    }
+
+    operator fun iterator(): MutableIterator<MutableMap.MutableEntry<String, String>> {
+        return table.iterator()
     }
 }
